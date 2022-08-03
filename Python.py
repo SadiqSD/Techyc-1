0a96a4c92a89e64d94c6ff60fef3ef356179109d
SadiqSD <sadiqmurtala20@gmail.com>
2022-08-03 19:21:27 +0100

    New repository

---
+++ Python.py
@@ -0,0 +1,3 @@
+#My name is sadiq murtala
+#How nice would it be if you fucking find something to do in your life mate
+#you only have this chance right now it you don't know ok?
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Techyc-1
+Dude life is so good
 FIRST program in company techt
 new others will be created in fueuture
