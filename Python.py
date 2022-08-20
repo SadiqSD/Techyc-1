aa6a02f944bd288a4f429eab2540e849dd5a75b3
SadiqSD <sadiqmurtala20@gmail.com>
2022-08-20 15:49:39 +0100

    PERSONAL INFO CM

--- Python.py
+++ Python.py
@@ -1,10 +1,27 @@
-#My name is sadiq murtala
-#How nice would it be if you fucking find something to do in your life mate
-#you only have this chance right now it you don't know ok?
-# import pandas as pd
-# dict = {"name" : ["sadiq","ibrahim","isa"], "age" : [1,2,3,4]}
-# print(dict)
-##### Continue
+# Normal format
+
+user_name = input("what is your name? ")                                        #asking questions
+
+while user_name:
+    print(user_name)
+else:
+    print("not username")
 
 
 
+
+
+
+
+# A program that will ask your name correct if something like
+######  Characters of name,empty entry,specail character
+# It might even help you and give you a reference/suggestion
+# the program will be design to ask you three times if nothing is writting within these three trials then you're out and terminated.
+# it will store it after all the process. The processes include
+# asking name, age ,date of birth, Nin, phonenumber,
+# after that it will store your data in deferent format they include
+# file ".doc extention", "txt", xlsx or something
+# or you can choose and independent format for you to store your infor after answering the questions
+# when everything is done and your you press enter/return it will prompt your "saved sucessfully."
+# if process is succesfull and saved, tthen we will proced in creating it with the GUI and continue to add some advance fueatures
+# The type of GUI to use will be selected base on the file format(Not sure yet)
